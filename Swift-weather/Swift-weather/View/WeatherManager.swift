66ef23b2f40bae3dbc51f9e29025ad1ea126fcaf
Sahar11 <saharhalim151@gmail.com>
2022-11-27 15:37:22 -0500

    add for weather conditions

--- Swift-weather/Swift-weather/View/WeatherManager.swift
+++ Swift-weather/Swift-weather/View/WeatherManager.swift
@@ -14,6 +14,8 @@ struct WeatherManager {
         let urlString = "\(weatherURL)&q=\(cityName)"
         performRequest(urlString: urlString)
     }
+    
+    // Get data
     func performRequest(urlString: String) {
         // Create URL
         
@@ -38,14 +40,38 @@ struct WeatherManager {
             
         }
     }
+    
+    // parse data
     func parseJSON(weatherData: Data) {
         let decoder = JSONDecoder()
         do {
             let decodedData = try decoder.decode(WeatherData.self, from: weatherData)
-            print(decodedData.name)
+            let id = decodedData.weather[0].id
+            print(getConditionName(weatherId: id))
         } catch {
             print(error)
         }
     }
     
+    func getConditionName(weatherId: Int) -> String {
+        switch weatherId {
+                case 200...232:
+                    return "cloud.bolt"
+                case 300...321:
+                    return "cloud.drizzle"
+                case 500...531:
+                    return "cloud.rain"
+                case 600...622:
+                    return "cloud.snow"
+                case 701...781:
+                    return "cloud.fog"
+                case 800:
+                    return "sun.max"
+                case 801...804:
+                    return "cloud.bolt"
+                default:
+                    return "cloud"
+                }
+        
+    }
 }
